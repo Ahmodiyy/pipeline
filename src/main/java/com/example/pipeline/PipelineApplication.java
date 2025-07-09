cfe3794c29b6c84a8b08fb1985a0c8e38f980c06
Ahmodiyy <ahmodolaitan03@gmail.com>
2025-07-09 13:55:09 +0100

    constans

--- src/main/java/com/example/pipeline/PipelineApplication.java
+++ src/main/java/com/example/pipeline/PipelineApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PipelineApplication {
-	private static final String CONSTANT = "constant";
+	private static final String CONSTANT = "constans";
 	public static void main(String[] args) {
 		SpringApplication.run(PipelineApplication.class, args);
 	}
