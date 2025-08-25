2be0479aae569bec11e0eb6736646eec32417aa9
Ahmodiyy <ahmodolaitan03@gmail.com>
2025-08-25 16:52:34 +0100

    patch

--- src/main/java/com/example/pipeline/Calculator.java
+++ src/main/java/com/example/pipeline/Calculator.java
@@ -1,10 +1,21 @@
 package com.example.pipeline;
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 @Service
 public class Calculator {
+
+    //@Cacheable("sum")
     public int sum(int a, int b) {
+        /**
+        try {
+            Thread.sleep(3000);
+        }
+        catch (InterruptedException e) {
+            e.printStackTrace();
+        } **/
+
         return a + b;
     }
     public int minus(int a, int b) {
--- src/main/java/com/example/pipeline/PipelineApplication.java
+++ src/main/java/com/example/pipeline/PipelineApplication.java
@@ -1,13 +1,23 @@
 package com.example.pipeline;
 
+//import com.hazelcast.client.config.ClientConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+//@EnableCaching
 public class PipelineApplication {
 	private static final String CONSTANT = "constant";
 	public static void main(String[] args) {
 		SpringApplication.run(PipelineApplication.class, args);
 	}
 
+	/**@Bean
+	public ClientConfig hazelcastClientConfig() {
+		ClientConfig clientConfig = new ClientConfig();
+		clientConfig.getNetworkConfig().addAddress("hazelcast");
+		return clientConfig;
+	}**/
 }
